e6cd3e5456df353a4aab84edbc56572b2eb4755d
Kertu Kipper <kertu.kipper@proekspert.ee>
2019-06-11 16:21:42 +0300

    Add HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "../styles/main.css";
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div>
+          <h1 className="header__title">Proekspert</h1>
+        </div>
+        <p className="header_text">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
